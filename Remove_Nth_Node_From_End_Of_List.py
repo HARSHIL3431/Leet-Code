d748955958b282c6f51bc6f68f737fbd5e802249
Harshil <harshilthakkar3435@gmail.com>
2026-02-12 21:40:34 +0530

    Day 16: Completed LC 21 & 19 using dummy node and two-pointer techniques.

---
+++ Remove_Nth_Node_From_End_Of_List.py
@@ -0,0 +1,26 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        
+        dummy = ListNode(0, head)
+        fast = dummy
+        slow = dummy
+        
+        # Move fast n steps ahead
+        for _ in range(n):
+            fast = fast.next
+        
+        # Move both pointers
+        while fast.next:
+            fast = fast.next
+            slow = slow.next
+        
+        # Remove node
+        slow.next = slow.next.next
+        
+        return dummy.next
